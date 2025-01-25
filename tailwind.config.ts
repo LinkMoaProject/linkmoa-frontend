9bbbf62fe5010c60f0bda5c243e6489167731a7c
sohyoeun <she020108@gmail.com>
2025-01-25 17:00:45 +0900

    style: 폰트 설정, tailwind theme 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        white: "#FFFFFF",
+        grey: {
+          100: "#F7F7F8",
+          200: "#CBCBCC",
+          300: "#8E8E8F",
+          400: "#59595B",
+          500: "#37373A",
+          600: "#28282B",
+        },
+        black: "#222222",
+        point: "#FF5500",
       },
     },
   },
